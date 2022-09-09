edb539d3369eeaac5b85edb39a40ff34dd202216
alopsantos <anderson_lops@hotmail.com>
2022-09-09 17:52:43 -0300

    :hammer: Refactor code menu header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,74 +1,119 @@
-import className from "classnames";
 import Logo from "./LogoAMB";
-import { Moon, SquaresFour, X } from "phosphor-react";
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { List, X } from "phosphor-react";
+import classNames from "classnames";
 
 export function Header() {
   const [open, setOpen] = useState(false);
-  const [scrollHeader, setScrollHeader] = useState(true);
-
-  const changeScrollHeader = () => {
-    window.screenY >= 100 ? setScrollHeader(true) : setScrollHeader(false);
-  };
-
-  useEffect(() => {
-    window.addEventListener("scroll", changeScrollHeader);
-  }, []);
 
   return (
-    <header className="fixed top-0 left-0 w-full h-20 items-center p-4 z-50 bg-white">
-      <nav className="container m-auto flex items-center justify-between text-gray-700">
-        <a href="#">
-          <Logo />
-        </a>
-
-        <div
-          className={className(
-            "gap-10 items-center md:pt-0 w-full md:flex-row md:h-12 md:right-0 opacity-0 md:opacity-100 ",
-            {
-              "block fixed bg-white top-0 right-0 h-full w-8/12 border opacity-100 pt-24 transition-all duration-300 justify-start":
-                open,
-              "flex-col hidden md:block transition-all duration-300": !open,
-            }
-          )}
-        >
-          <ul className="flex flex-col gap-4 md:pl-12 items-center md:flex-row md:gap-1 text-left">
-            <li className="hover:text-gray-400 py-4 px-6">
-              <a href="#" className="font-sans" title="">
-                Novidades
-              </a>
-            </li>
-            <li className="hover:text-gray-400 py-4 px-6">
-              <a href="#" className="font-sans" title="">
-                Coleções
-              </a>
-            </li>
-            <li className="hover:text-gray-400 py-4 px-6">
-              <a href="#" className="font-sans" title="">
-                Promoções
-              </a>
-            </li>
-            <li className="hover:text-gray-400 py-4 px-6">
-              <a href="#" className="font-sans" title="">
-                Contato
-              </a>
-            </li>
-            <X
-              size={32}
-              className="block cursor-pointer absolute top-7 right-8 md:hidden"
-              onClick={() => setOpen(!open)}
-            />
-          </ul>
+    <header className="containerm-auto fixed top-0 left-0 w-full h-20 items-center p-4 z-50 bg-white mb-2 md:mb-5">
+      <div className="flex max-w-5xl gap-4 mx-auto items-center justify-between text-gray-700">
+        <div className="ml-4 md:ml-0">
+          <Link to="/">
+            <Logo />
+          </Link>
         </div>
-        <div className="flex items-center gap-4">
-          <button
-            className="block md:hidden py-4 mx-2 rounded focus:outline-none group"
-            onClick={() => setOpen(!open)}
+        <nav className="">
+          <div className="hidden md:flex">
+            <ul className="flex gap-3">
+              <li>
+                <Link
+                  className="text-lg text-black hover:text-gray-500 transition-all duration-300"
+                  to="#"
+                >
+                  Coleções
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="text-lg text-black hover:text-gray-500 transition-all duration-300"
+                  to="#"
+                >
+                  Produtos
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="text-lg text-black hover:text-gray-500 transition-all duration-300"
+                  to="#"
+                >
+                  Atendimento
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="text-lg text-black hover:text-gray-500 transition-all duration-300"
+                  to="#"
+                >
+                  Sobre nós
+                </Link>
+              </li>
+            </ul>
+          </div>
+
+          <div
+            className={classNames(
+              "fixed top-0 w-[70%] min-h-screen bg-white border-x-gray-800 drop-shadow-sm transition-all duration-300",
+              {
+                "right-[-100%]": open === true,
+                "right-[0] fixed": open === false,
+              }
+            )}
           >
-            <SquaresFour size={28} />
+            <div className="flex p-4">
+              <div className="flex w-[100%] p-6">
+                <ul className="flex flex-col gap-3">
+                  <li>
+                    <Link
+                      className="text-lg text-black hover:text-gray-500 transition-all duration-300"
+                      to="#"
+                    >
+                      Coleções
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      className="text-lg text-black hover:text-gray-500 transition-all duration-300"
+                      to="#"
+                    >
+                      Produtos
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      className="text-lg text-black hover:text-gray-500 transition-all duration-300"
+                      to="#"
+                    >
+                      Atendimento
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      className="text-lg text-black hover:text-gray-500 transition-all duration-300"
+                      to="#"
+                    >
+                      Sobre nós
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+              <div>
+                <button onClick={() => setOpen(!open)}>
+                  <X size={25} />
+                </button>
+              </div>
+            </div>
+          </div>
+        </nav>
+
+        <div className="md:hidden">
+          <button onClick={() => setOpen(!open)}>
+            <List size={25} />
           </button>
         </div>
-      </nav>
+      </div>
     </header>
   );
 }
