24c876debb79358fff0698cc923ea8a5e234aa99
alopsantos <anderson_lops@hotmail.com>
2022-09-12 17:14:41 -0300

    :bug: Name

--- src/components/Vendedor.tsx
+++ src/components/Vendedor.tsx
@@ -13,6 +13,67 @@ export function Vendedor() {
               <a
                 href="#"
                 className="hover:text-gray-600 transition-all duration-300 group flex flex-col items-center"
+                title="Vendedora Ivone"
+              >
+                <div className="absolute bottom-12 right-8 p-[2px] text-center  rounded-2xl bg-white opacity-70">
+                  <WhatsappLogo size={20} className="text-green-900" />
+                </div>
+                <img
+                  src="/images/jessyca.jpg"
+                  alt=""
+                  className="w-[100px] rounded-[50%] group-hover:border-[1px] group-hover:border-black border-[1px] transition-all duration-300"
+                />
+                <div className="flex flex-col text-center">
+                  <h3 className="text-lg">Jessyca</h3>
+                  <span className="text-sm">Sta Terezinha Itaipu - PR</span>
+                </div>
+              </a>
+            </li>
+
+            <li className="relative">
+              <a
+                href="#"
+                className="hover:text-gray-600 transition-all duration-300 group flex flex-col items-center"
+                title="Vendedora Ivone"
+              >
+                <div className="absolute bottom-12 right-8 p-[2px] text-center  rounded-2xl bg-white opacity-70">
+                  <WhatsappLogo size={20} className="text-green-900" />
+                </div>
+                <img
+                  src="/images/pamela.jpg"
+                  alt=""
+                  className="w-[100px] rounded-[50%] group-hover:border-[1px] group-hover:border-black border-[1px] transition-all duration-300"
+                />
+                <div className="flex flex-col text-center">
+                  <h3 className="text-lg">Pamela</h3>
+                  <span className="text-sm">Sta Terezinha Itaipu - PR</span>
+                </div>
+              </a>
+            </li>
+            <li className="relative">
+              <a
+                href="#"
+                className="hover:text-gray-600 transition-all duration-300 group flex flex-col items-center"
+                title="Vendedora Ivone"
+              >
+                <div className="absolute bottom-12 right-8 p-[2px] text-center  rounded-2xl bg-white opacity-70">
+                  <WhatsappLogo size={20} className="text-green-900" />
+                </div>
+                <img
+                  src="/images/flaviane.jpg"
+                  alt=""
+                  className="w-[100px] rounded-[50%] group-hover:border-[1px] group-hover:border-black border-[1px] transition-all duration-300"
+                />
+                <div className="flex flex-col text-center">
+                  <h3 className="text-lg">Flaviane</h3>
+                  <span className="text-sm">Sta Terezinha Itaipu - PR</span>
+                </div>
+              </a>
+            </li>
+            <li className="relative">
+              <a
+                href="#"
+                className="hover:text-gray-600 transition-all duration-300 group flex flex-col items-center"
                 title="Vendedora Kety"
               >
                 <div className="absolute bottom-12 right-8 p-[2px] text-center  rounded-2xl bg-white opacity-70">
@@ -47,67 +108,6 @@ export function Vendedor() {
                 <div className="flex flex-col text-center">
                   <h3 className="text-lg">Ivone</h3>
                   <span className="text-sm">Bom Retiro - SP</span>
-                </div>
-              </a>
-            </li>
-            <li className="relative">
-              <a
-                href="#"
-                className="hover:text-gray-600 transition-all duration-300 group flex flex-col items-center"
-                title="Vendedora Ivone"
-              >
-                <div className="absolute bottom-12 right-8 p-[2px] text-center  rounded-2xl bg-white opacity-70">
-                  <WhatsappLogo size={20} className="text-green-900" />
-                </div>
-                <img
-                  src="/images/jessyca.jpg"
-                  alt=""
-                  className="w-[100px] rounded-[50%] group-hover:border-[1px] group-hover:border-black border-[1px] transition-all duration-300"
-                />
-                <div className="flex flex-col text-center">
-                  <h3 className="text-lg">Jessyca</h3>
-                  <span className="text-sm">Sta Terezinha Itaipu - PR</span>
-                </div>
-              </a>
-            </li>
-
-            <li className="relative">
-              <a
-                href="#"
-                className="hover:text-gray-600 transition-all duration-300 group flex flex-col items-center"
-                title="Vendedora Ivone"
-              >
-                <div className="absolute bottom-12 right-8 p-[2px] text-center  rounded-2xl bg-white opacity-70">
-                  <WhatsappLogo size={20} className="text-green-900" />
-                </div>
-                <img
-                  src="/images/flaviane.jpg"
-                  alt=""
-                  className="w-[100px] rounded-[50%] group-hover:border-[1px] group-hover:border-black border-[1px] transition-all duration-300"
-                />
-                <div className="flex flex-col text-center">
-                  <h3 className="text-lg">Flaviane</h3>
-                  <span className="text-sm">Sta Terezinha Itaipu - PR</span>
-                </div>
-              </a>
-            </li>
-            <li className="relative">
-              <a
-                href="#"
-                className="hover:text-gray-600 transition-all duration-300 group flex flex-col items-center"
-                title="Vendedora Ivone"
-              >
-                <div className="absolute bottom-12 right-8 p-[2px] text-center  rounded-2xl bg-white opacity-70">
-                  <WhatsappLogo size={20} className="text-green-900" />
-                </div>
-                <img
-                  src="/images/pamela.jpg"
-                  alt=""
-                  className="w-[100px] rounded-[50%] group-hover:border-[1px] group-hover:border-black border-[1px] transition-all duration-300"
-                />
-                <div className="flex flex-col text-center">
-                  <h3 className="text-lg">Pamlea</h3>
-                  <span className="text-sm">Sta Terezinha Itaipu - PR</span>
                 </div>
               </a>
             </li>
